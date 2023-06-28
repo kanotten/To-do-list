c5bfc00992ccd6bead8c3ec595a770b7ba63afb3
Sheikh <kenneth.sheikh@hotmail.com>
2023-06-28 15:53:10 +0200

    changed standard checkbox to toggle switches. resized to fit the tasklist words

--- script.js
+++ script.js
@@ -4,44 +4,32 @@ const taskList = document.getElementById("taskList");
 
 addButton.addEventListener("click", addTask);
 taskInput.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    addTask();
-  }
+    if (event.key === "Enter") {
+        event.preventDefault();
+        addTask();
+    }
 });
 
 function addTask() {
-  const taskText = taskInput.value;
+    const taskText = taskInput.value;
 
-  if (taskText) {
-    const listItem = document.createElement("li");
-    const taskLabel = document.createElement("label");
-    const taskCheckbox = document.createElement("input");
-    taskCheckbox.type = "checkbox";
-    const taskTextSpan = document.createElement("span");
-    taskTextSpan.textContent = taskText;
-    taskLabel.appendChild(taskCheckbox);
-    taskLabel.appendChild(taskTextSpan);
-    listItem.appendChild(taskLabel);
-    listItem.classList.add("task");
-    taskList.appendChild(listItem);
+    if (taskText) {
+        const listItem = document.createElement("li");
+        const taskLabel = document.createElement("label");
+        const switchCheckbox = document.createElement("input");
+        switchCheckbox.type = "checkbox";
+        switchCheckbox.classList.add("switch-checkbox");
+        const switchSlider = document.createElement("span");
+        switchSlider.classList.add("switch-slider");
+        taskLabel.appendChild(switchCheckbox);
+        taskLabel.appendChild(switchSlider);
+        const taskTextSpan = document.createElement("span");
+        taskTextSpan.textContent = taskText;
+        taskLabel.appendChild(taskTextSpan);
+        listItem.appendChild(taskLabel);
+        listItem.classList.add("task");
+        taskList.appendChild(listItem);
 
-    taskInput.value = "";
-  }
+        taskInput.value = "";
+    }
 }
-
-taskList.addEventListener("change", function (event) {
-  if (event.target.matches("input[type='checkbox']")) {
-    const checkbox = event.target;
-    const listItem = checkbox.parentNode;
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    svg.setAttribute("class", "checkbox-svg");
-    svg.setAttribute("viewBox", "0 0 24 24");
-    path.setAttribute("d", "M4,12 L9,15 L20,4");
-    path.setAttribute("class", "checkbox-stroke");
-    svg.appendChild(path);
-    listItem.insertBefore(svg, listItem.firstChild);
-    checkbox.style.display = "none";
-  }
-});
